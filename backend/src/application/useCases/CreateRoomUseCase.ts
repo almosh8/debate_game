6385a9c381366e6cbdca8065259ce65f14a94d53
almosh8 <mr.almosh443@mail.ru>
2025-03-03 21:27:26 +0300

    adding room creator to players list

--- backend/src/application/useCases/CreateRoomUseCase.ts
+++ backend/src/application/useCases/CreateRoomUseCase.ts
@@ -1,5 +1,6 @@
 // src/application/useCases/CreateRoomUseCase.ts
 import { Room } from "../../domain/Room";
+import { Player } from "../../domain/Player";
 import { IRoomRepository } from "../interfaces/IRoomRepository";
 import { Logger } from "../../utils/Logger";
 import { v4 as uuid } from "uuid";
@@ -11,20 +12,14 @@ export class CreateRoomUseCase {
     this.logger.info("CreateRoomUseCase initialized");
   }
 
-  private isAdminIP(ip: string): boolean {
-    const adminIPs = process.env.ADMIN_IPS?.split(",") || [];
-    return adminIPs.includes(ip);
-  }
-
   async execute(adminId: string, userIP: string): Promise<Room> {
     this.logger.info(`Attempt to create room by IP: ${userIP}`);
 
-    if (!this.isAdminIP(userIP)) {
-      this.logger.warn(`Access denied for IP: ${userIP}`);
-      throw new Error("Access denied: IP is not in the admin list");
-    }
-
+    // Создаем комнату с админом на Seat 1
     const room = new Room(uuid(), adminId, [], "waiting");
+    const adminPlayer = new Player(uuid(), adminId, "admin", 1); // Админ на Seat 1
+    room.players.push(adminPlayer);
+
     await this.roomRepository.save(room);
     this.logger.info(`Room created: ${room.id}`);
     return room;
